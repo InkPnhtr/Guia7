fa9d6e8ebfe4c36bdc1b3aefa9364ce567793e2b
Nr <Nr@tbc.com>
2023-05-02 00:41:07 -0300

    Suma E3

---
+++ Ej3G7/src/ej3g7/Ej3G7.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ej3g7;
+
+/**
+ *
+ * @author U
+ */
+public class Ej3G7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Operacion Op1 = new Operacion();
+        Op1.crearOperacion();
+        System.out.println("Suma = " + Op1.sumar());
+    }
+    
+}
---
+++ Ej3G7/src/ej3g7/Operacion.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ej3g7;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author U
+ */
+public class Operacion {
+    private int numero1;
+    private int numero2;
+
+    public Operacion(int numero1, int numero2) {
+        this.numero1 = numero1;
+        this.numero2 = numero2;
+    }
+
+    public Operacion() {
+    }
+
+    public int getNumero1() {
+        return numero1;
+    }
+
+    public int getNumero2() {
+        return numero2;
+    }
+
+    public void setNumero1(int numero1) {
+        this.numero1 = numero1;
+    }
+
+    public void setNumero2(int numero2) {
+        this.numero2 = numero2;
+    }
+    public void crearOperacion(){
+        Scanner leer = new Scanner(System.in);
+        System.out.print("Ing un Ent. A: ");
+        setNumero1(leer.nextInt());
+        System.out.print("Ing un Ent. B: ");
+        setNumero2(leer.nextInt());
+    }
+    public int sumar(){
+        int suma = getNumero1() + getNumero2();
+        return suma;
+    }
+}
